4a386831ff04e3431c5232cdf48ba6706b046118
Anshu Raj <rajanshu2233@gmail.com>
2026-09-12 15:56:33 +0530

    Time: 0 ms (100%), Space: 22.9 MB (15.9%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    unordered_map<int, int> mp;
+    int maxD = 0;
+
+    TreeNode* LCA(TreeNode* root)
+    {
+        if(root == NULL || mp[root->val] == maxD)
+        {
+            return root;
+        }
+
+        TreeNode* l = LCA(root->left);
+        TreeNode* r = LCA(root->right);
+
+        if(l != NULL && r != NULL)
+        {
+            return root;
+        }
+
+        if(l != NULL)
+        {
+            return l;
+        }
+
+        return r;
+    }
+
+    void depth(TreeNode* root, int d)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+
+        maxD = max(maxD, d);
+        mp[root->val] = d;
+
+        depth(root->left, d + 1);
+        depth(root->right, d + 1);
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root)
+    {
+        depth(root, 0);
+
+        return LCA(root);
+    }
+};
